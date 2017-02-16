dbdb71a73a02f5e4438e91a93bc8121211caf2c3
domis4 <dominik.stoetter@yahoo.com>
2017-02-16 20:37:35 +0100

    add basic grid functionality

--- src/app/game-of-life/grid/grid.component.ts
+++ src/app/game-of-life/grid/grid.component.ts
@@ -6,20 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./grid.component.css']
 })
 export class GridComponent implements OnInit {
-  gridSize = [32, 32];
+  gridSize = [12, 12];
   grid = [[]];
   constructor() { }
 
   ngOnInit() {
-    this.reinitGrid();
+    this.reinitGrid('random');
   }
 
-  reinitGrid() {
+  switchState(x: number, y: number) {
+    if (this.grid[x][y] === 0) {
+      this.grid[x][y] = 1;
+    } else {
+      this.grid[x][y] = 0;
+    }
+  }
+
+  reinitGrid(mode: string) {
     this.grid = [];
     for (let i = 0; i < this.gridSize[0]; i++) {
       this.grid[i] = [];
       for (let j = 0; j < this.gridSize[1]; j++) {
-        this.grid[i][j] = 0;
+        if (mode === 'random') {
+          this.grid[i][j] = Math.round(Math.random());
+        }
+        if (mode === 'empty') {
+          this.grid[i][j] = 0;
+        }
       }
     }
   }
